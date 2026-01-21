3d5871c321f847a3f9180032d925043a0e7a770a
YouNa17 <futiany986@gmail.com>
2026-01-22 08:40:26 +0900

    cors設定

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,9 +6,21 @@ from fastapi import FastAPI
 from .routers import books
 from .database import engine, Base
 from . import models
+from fastapi.middleware.cors import CORSMiddleware
 
 # FastAPIのアプリを作る
 app = FastAPI()
+
+# cors設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",  # Next.js
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # FIXME:アプリ起動時にテーブルがなければ作る処理 => いずれAlembic
 Base.metadata.create_all(bind=engine)
